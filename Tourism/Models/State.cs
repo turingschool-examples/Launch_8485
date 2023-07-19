48713fab89967e22af299264b6365ae29243a240
Richard T <richard@turing.edu>
2023-07-19 14:53:04 -0400

    Update model to set list to new empty list

--- Tourism/Models/State.cs
+++ Tourism/Models/State.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Abbreviation { get; set; }
-        public List<City> Cities { get; set; }
+        public List<City> Cities { get; set; } = new List<City>();
     }
 }
